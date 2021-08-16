4aa227aec442ba016ff2d277d8f32c8cb82c711e
jkjh8 <jkjh82@naver.com>
2021-08-16 20:57:02 +0900

    로그 조회

--- routes/eventlog/index.js
+++ routes/eventlog/index.js
@@ -4,21 +4,24 @@ const dbLogs = require('../../models').Logs
 
 router.get('/', (req, res) => {
   const { limit, page, serarch, location } = req.query
-  const offset = limit * page - 1
+  const offset = limit * (page - 1)
+  console.log(limit)
+  console.log(offset)
   dbLogs.findAndCountAll({
-    limit: limit ?? 10,
-    offset: page ?? 0,
+    limit: Number(limit),
+    offset: offset,
     order: [
       ['createdAt', 'DESC']
     ]
   }).then((result) => {
     console.log(result)
     res.status(200).json({
-      logsPerPage: limit ?? 10,
-      page: page ?? 0,
+      logsPerPage: limit,
+      page: page,
       data: result
     })
   }).catch((err) => {
+    console.log(err)
     res.status(500).json({ error: err })
   })
 })
