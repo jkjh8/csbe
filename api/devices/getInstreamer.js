3f4349d40b0bda8ac5f0e2c8d75fec30af20ca99
jkjh8 <jkjh82@naver.com>
2021-08-28 01:10:45 +0900

    barix

--- api/devices/getInstreamer.js
+++ api/devices/getInstreamer.js
@@ -3,20 +3,40 @@ const cheerio = require('cheerio')
 
 const getHtml = async () => {
   try {
-    return await axios.get('http://192.168.1.46/refresh')
+    return await axios.get('http://192.168.1.46/status')
   } catch (error) {
     console.error(error)
   }
 }
 getHtml().then(html => {
-  console.log(html.data)
+  hardware = new Object()
+  status = new Object()
+  const $ = cheerio.load(html.data)
+  $('dd').each(function(index, ele) {
+    if ($(this).find('#hardware').attr('class')) {
+      hardware[$(this).find('#hardware').attr('class')] = $(this).find('#hardware').text().trim()
+    }
+    if ($(this).find('#status').attr('class')) {
+      status[$(this).find('#status').attr('class')] = $(this).find('#status').text().trim()
+    }
+  })
+  
+
+  $('dd').each(function(index, ele) {
+    
+    
+  })
+  const r = {
+    hardware: hardware,
+    status: status
+  }
+  console.log(r)
+  // console.log($bodyList.length)
+  // console.log($bodyList.html())
+  
+  // const info = $bodyList.children('#hardware')
+  // console.log(info.length, info.text())
+  // for(let i = 0; 0 < info.length; i++) {
+  //   console.log((info).eq(i))
+  // }
 })
-
-// getHtml().then(html => {
-//   console.log(html.data)
-//   const $ = cheerio.load(html.data)
-//   const $bodyList = $('font > dl > dd')
-//   console.log($($bodyList).text())
-// })
-
-
