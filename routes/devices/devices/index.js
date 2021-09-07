73074c9e333097c0b223d02cf6f11c2935d5a5b1
jkjh8 <jkjh82@naver.com>
2021-09-07 16:21:23 +0900

    1234

--- routes/devices/devices/index.js
+++ routes/devices/devices/index.js
@@ -20,6 +20,16 @@ router.get('/', async function (req, res) {
   }
 })
 
+async function deleteDevices (obj) {
+  try {
+    await Qsys.deleteMany({ ipaddress: obj.ipaddress })
+    await Barixes.deleteMany({ ipaddress: obj.ipaddress})
+    return null
+  } catch (err) {
+    return err
+  }
+}
+
 router.post('/', async function (req, res) {
   const info = req.body
   try {
@@ -27,17 +37,14 @@ router.post('/', async function (req, res) {
     const checkMessage = await check(info)
     if (checkMessage) return res.status(500).json({ message: checkMessage })
     // qsys 생성
-    if (info.type === 'QSys') {
-      createQsys(info)
-    }
+    if (info.type === 'QSys') { createQsys(info) }
     // barix 생성 안함 데이터 갱신시 자동 등록
-
     // 디바이스 생성 및 저장
     const device = new Devices(info)
     device.save().then(async (doc) => {
       return res.status(200).json({ data: doc })
     }).catch( async (err) => {
-      await Qsys.deleteMany({ ipaddress: info.ipaddress })
+      await deleteDevices(info)
       return res.status(500).json({ message: '데이터 베이스 오류가 발생하였습니다.', data: err })
     })
   } catch (err) {
@@ -54,19 +61,9 @@ router.put('/', async function (req, res) {
     if (checkMessage) return res.status(500).json({ message: checkMessage })
 
     // qsys 확인
-    if (info.type === 'QSys') {
-      editQsys(info)
-    }
-
+    if (info.type === 'QSys') { editQsys(info) }
     info.ckecked = true
-    delete info.updatedAt
-
-    r = await Devices.updateOne({
-      _id: info._id
-    }, {
-      $set: info
-    })
-
+    const r = await Devices.updateOne({ _id: info._id }, { $set: info })
     res.status(200).json({ data: r })
   } catch (err) {
     res.status(500).json({ status: 'error', data: err, message: ' 알 수 없는 오류가 발생하였습니다.' })
@@ -76,8 +73,7 @@ router.put('/', async function (req, res) {
 router.get('/delete', async (req, res) => {
   try {
     let r = await Devices.deleteOne({ ipaddress: req.query.ipaddress })
-    await Barixes.deleteOne({ ipaddress: req.query.ipaddress })
-    await Qsys.deleteOne({ ipaddress: req.query.ipaddress })
+    await deleteDevices(req.query)
     res.status(200).json(r)
   } catch (err) {
     console.log(err)
